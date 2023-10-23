67fb8c66ffa76ce5beadce3437cbd803a0dbec89
David Cruz <df.cruza@uniandes.edu.co>
2023-10-23 13:53:09 -0500

    rick roll

--- front/src/App.js
+++ front/src/App.js
@@ -71,6 +71,7 @@ function App() {
               </Col>
             </Row>
           </Container>
+          <iframe width="560" height="315" src="https://youtu.be/BBJa32lCaaY?si=hUCoAUrSjKoaNQrp&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </Form>
       )}
     </div>
--- front/src/index.js
+++ front/src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import Footer from './footer';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Footer />
   </React.StrictMode>
 );
 
