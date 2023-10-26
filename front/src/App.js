7883edf3fa0e3e6092d1fbed72a54053449e3b70
Sergio Guillen <sa.guillen@uniandes.edu.co>
2023-10-25 21:08:57 -0500

    Desarrollo del front

--- front/src/App.js
+++ front/src/App.js
@@ -32,22 +32,68 @@ function App() {
       });
   };
 
-    const textosMostrar=respuestas.map(
-        (respuesta)=>{
-            return(
-                <tr>
-                    <td>{respuesta.text}</td>
-                    <td>{respuesta.sdg}</td>
-                </tr>
-            )
-        }
-    )
+
+
+  const textosMostrar = respuestas.map(
+    (respuesta) => {
+      return (
+        <tr>
+          <td>{respuesta.text}</td>
+          <td>{respuesta.sdg}</td>
+          <td><div class="flip-box">
+            {(() => {
+              switch (respuesta.sdg) {
+                case 3:
+                  return (
+                    <div class="flip-box-inner">
+
+                      <div class="flip-box-front">
+                        <img src="https://www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2018/07/S_SDG-goals_icons-individual-rgb-03.png" width="200" height="200" alt="" />
+                      </div>
+                      <div class="flip-box-back3">
+                        <p>Garantizar una vida sana y promover el bienestar para todos en todas las edades</p>
+                      </div>
+                    </div>);
+                case 4:
+                  return (
+                    <div class="flip-box-inner">
+
+                      <div class="flip-box-front">
+                        <img src="https://www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2018/07/S_SDG-goals_icons-individual-rgb-04.png" width="200" height="200" alt="" />
+                      </div>
+                      <div class="flip-box-back4">
+                        <p>Garantizar una educación inclusiva, equitativa y de calidad y promover oportunidades de aprendizaje durante toda la vida para todos</p>
+                      </div>
+                    </div>);
+                case 5:
+                  return (
+                    <div class="flip-box-inner">
+
+                      <div class="flip-box-front">
+                        <img src="https://www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2018/07/S_SDG-goals_icons-individual-rgb-05.png" width="200" height="200" alt="" />
+                      </div>
+                      <div class="flip-box-back5">
+                        <p>Lograr la igualdad entre los géneros y empoderar a todas las mujeres y las niñas</p>
+                      </div>
+                    </div>);
+                default:
+                  return null;
+              }
+            })()}
+
+          </div>
+          </td>
+        </tr>
+      )
+    }
+  )
 
   return (
     <div className="App">
-      <h1>Objetivos de desarrollo sostenible</h1>
+      <img src="https://www.un.org/sustainabledevelopment/es/wp-content/uploads/sites/3/2019/09/S_SDG_logo_without_UN_emblem_horizontal_WEB-1024x134.png" alt="Banner" />
+      <h1>Herramienta para predecir Objetivos de desarrollo sostenible</h1>
       <Form onSubmit={handleSubmit}>
-        <Container style={{textAlign: "left", paddingTop:"5vh"}}>
+        <Container style={{ textAlign: "left", paddingTop: "5vh" }}>
           <Row>
             <Col>
               <Form.Group controlId="formBasicEmail">
@@ -78,7 +124,7 @@ function App() {
               </tr>
             </thead>
             <tbody>
-            {textosMostrar}
+              {textosMostrar}
             </tbody>
           </Table>
           <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=SqyQ7ctzykF1R4hG&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
